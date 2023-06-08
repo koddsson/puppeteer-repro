cdeff89bf768582b1693cdd3559070932b843eeb
Kristján Oddsson <koddsson@gmail.com>
2023-06-08 09:48:02 +0200

    set headless: "new"

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({headless: false, devtools: true});
+  const browser = await puppeteer.launch({headless: 'new'});
   const page = await browser.newPage();
 
   await page.goto('https://koddsson.com');
